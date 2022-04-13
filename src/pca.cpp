b263ac7f33764daa828b97b263115bf5331a0d47
AvalosM <manuel.avalos.98@gmail.com>
2022-04-13 16:02:52 -0300

    Implemented PCA class, fit and transform

---
+++ src/pca.cpp
@@ -0,0 +1,38 @@
+#include "pca.hpp"
+#include "eigen.hpp"
+
+PCA::PCA(unsigned int alpha) : alpha_(alpha)
+{
+}
+
+void PCA::fit(Matrix data)
+{
+    /* Calculate median value for each pixel
+     *
+     *     1xn             nxm         1x1                         1xm
+     *
+     * [1, ..., 1] * [i00, ..., i0m] * 1/n = [(i00 + ... + in0)/n, ..., (i0m + ... + inm)/n]
+     *               [..., ..., ...]
+     *               [in0, ..., inm]
+     */
+    VectorXd mean_vector = data.colwise().mean();
+
+    /* Calculate covariance matrix
+     * 
+     * X = [data(0) - mean_vector]
+     *     [         ...         ]
+     *     [data(n) - mean_vector]
+     * 
+     * Mx = X^t * X / n - 1
+     */
+    data.rowwise() -= mean_vector;
+    Matrix Mx = data.transpose() * data / (data.rows() - 1);
+
+    /* Get first alpha eigenvalue/eigenvector pairs */
+    V_ = get_first_eigenvalues(Mx, alpha_, 5000, 1e-16).second;
+}
+
+Matrix PCA::transform(Matrix X)
+{
+    return X * V_.block(0,0,V_.rows(), alpha_);
+}
---
+++ src/pca.hpp
@@ -0,0 +1,20 @@
+#ifndef __PCA_HPP__
+#define __PCA_HPP__
+
+#include "types.hpp"
+
+class PCA {
+public:
+    PCA(unsigned int alpha);
+
+    void fit(Matrix data);
+
+    inline void setalpha(unsigned int alpha) { alpha_ = alpha; };
+
+    Matrix transform(Matrix X);
+private:
+    unsigned int alpha_;
+    Matrix V_;
+};
+
+#endif /* __PCA_HPP__ */
