694c8b8addad725ed1f85668fd189316feeebdc1
AvalosM <manuel.avalos.98@gmail.com>
2022-04-15 20:58:45 -0300

    kNN pybind11 module

---
+++ src/kNN-pybind11.cpp
@@ -0,0 +1,36 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/eigen.h>
+#include "kNN.hpp"
+#include "pca.hpp"
+#include "eigen.hpp"
+
+namespace py=pybind11;
+
+PYBIND11_MODULE(kNN, m) {
+    py::class_<KNNClassifier>(m, "KNNClassifier")
+        .def(py::init<unsigned int, unsigned int>())
+        .def("fit", &KNNClassifier::fit)
+        .def("setneighbors", &KNNClassifier::setneighbors)
+        .def("predict", &KNNClassifier::predict);
+    py::class_<PCA>(m, "PCA")
+        .def(py::init<unsigned int>())
+        .def("fit", &PCA::fit)
+        .def("setalpha", &PCA::setalpha)
+        .def("transform", &PCA::transform);
+    m.def(
+        "power_iteration", &power_iteration,
+        "Function that calculates the eigenvalue/eigenvector pair with max |eigval|)",
+        py::arg("X"),
+        py::arg("num_iter")=5000,
+        py::arg("epsilon")=1e-16
+    );
+    m.def(
+        "get_first_eigenvalues", &get_first_eigenvalues,
+        "Function that calculates first num eigenvalue/eigenvector pairs",
+        py::arg("X"),
+        py::arg("num"),
+        py::arg("num_iter")=5000,
+        py::arg("epsilon")=1e-16
+    );
+
+}
